9b6fa9940ccd598d9955b35013c87be516bb4be3
gata2825 <FuYa7226977@outlook.jp>
2026-06-15 16:46:45 +0900

    Add yamagata

--- main.cpp
+++ main.cpp
@@ -6,4 +6,5 @@ int main(void)
 	cout << "This is first commit from Kensaku Mori (2026/6/15)" << endl;
 	cout << "hello from Suzuha!" << endl;
 	cout << "hello from Ryo!" << endl;
+	cout << "hello from yamagata!" << endl;
 }
