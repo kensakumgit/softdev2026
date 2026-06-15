4e6c2cd0d173979dd70ee7443e3552c5addf2183
Riku <rikupuni06@icloud.com>
2026-06-15 18:07:41 +0900

    メッセージ

--- main.cpp
+++ main.cpp
@@ -3,14 +3,17 @@ using namespace std;
 
 int main(void)
 {
-	cout << "This is first commit from Kensaku Mori (2026/6/15)" << endl;
-	cout <<"Hello from kuborio!" <<endl;
-	cout << "hello from Suzuha!" << endl;
-	cout << "hello from Ryo!" << endl;
-	cout << "hello from yamagata!" << endl;
-	cout << "hello from haru!" << endl;
-	cout << "hello from harung0!" << endl;
-	cout << "hello from yasuda-shunki!" << endl;
-	cout << "hello from reiji!" << endl;
-	cout << "hello from terada-git!" << endl
+    cout << "This is first commit from Kensaku Mori (2026/6/15)" << endl;
+    cout << "Hello from kuborio!" << endl;
+    cout << "hello from Suzuha!" << endl;
+    cout << "hello from Ryo!" << endl;
+    cout << "hello from yamagata!" << endl;
+    cout << "hello from haru!" << endl;
+    cout << "hello from harung0!" << endl;
+    cout << "hello from yasuda-shunki!" << endl;
+    cout << "hello from reiji!" << endl;
+    cout << "hello from terada-git!" << endl;
+    cout << "hello from rikupuni!" << endl;
+
+    return 0;
 }
